c1e8d1ebf57d6d7283274358c865fefc90faf8cd
brewsterl <leandroprox@gmail.com>
2014-01-06 22:46:59 -0430

    Update arrow.lua

--- data/actions/scripts/rook/minotaur hell quest/arrow.lua
+++ data/actions/scripts/rook/minotaur hell quest/arrow.lua
@@ -1,13 +1,13 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 
-      if item.uid == 3020 then
-         queststatus = getPlayerStorageValue(cid,3020)
+      if item.uid == 11020 then
+         queststatus = getPlayerStorageValue(cid,11020)
          if queststatus == -1 then
             doPlayerSendTextMessage(cid,MESSAGE_INFO_DESCR, "You have found a bag.")
     container = doPlayerAddItem(cid,1987,1)
 	doAddContainerItem(container,2545,1)
 	doAddContainerItem(container,2544,1)
-            setPlayerStorageValue(cid,3020,1)
+            setPlayerStorageValue(cid,11020,1)
          else
             doPlayerSendTextMessage(cid,MESSAGE_INFO_DESCR, "It is empty.")
          end
