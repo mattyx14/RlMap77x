8b123822491edbc04e53ba4b053e09f067833032
Martin <martin_jojo@hotmail.com>
2014-01-06 19:41:29 -0800

    fix console error

--- data/actions/scripts/rook/goblin temple quest/goblin temple snow.lua
+++ data/actions/scripts/rook/goblin temple quest/goblin temple snow.lua
@@ -6,7 +6,7 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
             doPlayerSendTextMessage(cid,MESSAGE_INFO_DESCR, "You have found a bag.")
    container = doPlayerAddItem(cid, 1987, 1)
    doAddContainerItem(container,2111,4)
-   doAddContainerItem(container,1294,5
+   doAddContainerItem(container,1294,5)
             setPlayerStorageValue(cid,3014,1)
          else
             doPlayerSendTextMessage(cid,MESSAGE_INFO_DESCR, "It is empty.")
