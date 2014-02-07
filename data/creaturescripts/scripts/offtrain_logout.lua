c9c7552fdb74b3933e70c9fd1f28579a346d576a
brewsterl <leandroprox@gmail.com>
2014-02-07 15:24:08 -0430

    Remove old offline trainer
    bug with 7.4

--- data/creaturescripts/scripts/offtrain_logout.lua
+++
@@ -1,9 +0,0 @@
-function onLogout(cid)
-	if(OfflineTraining_canStartTraining(cid)) then
-		OfflineTraining_turnOnTraining(cid)
-		OfflineTraining_onStartTraining(cid)
-	end
-
-	OfflineTraining_setLogoutTime(cid)
-	return true
-end
